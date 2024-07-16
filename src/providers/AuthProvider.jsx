0b38fecfe9c28c4d862599d4d9c7b744fbf4a2ef
isaiful508 <isaiful.islam508@gmail.com>
2024-07-16 16:52:38 +0600

    login completed

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -21,7 +21,8 @@ const auth = getAuth(app)
 const googleProvider = new GoogleAuthProvider()
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
+  console.log(user);
   const [loading, setLoading] = useState(true);
   const axiosPublic = useAxiosPublic();
 
